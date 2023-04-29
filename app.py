51d93a2117b3a1b3e225c3bc075961b2ae9612bf
GNKSlash <105719872+GNKSlash@users.noreply.github.com>
2023-04-29 10:22:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ app = Flask(__name__)
 
 logs = []
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def index():
-    if request.method == 'POST':
-        log = request.form['log']
-        logs.append(log)
+    log = request.form['log']
+    logs.append(log)
     return render_template('index.html', logs=logs)
 
 if __name__ == '__main__':
